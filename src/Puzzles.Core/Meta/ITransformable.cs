5c2dde76b6dfd2a4acdd5163eaec3f40341d01a0
Kyouyama Kazusa <Kazusa0805@outlook.com>
2024-12-05 16:09:29 +0800

    Implements transforming methods.

--- src/Puzzles.Core/Meta/ITransformable.cs
+++ src/Puzzles.Core/Meta/ITransformable.cs
@@ -10,41 +10,55 @@ public interface ITransformable<TSelf> where TSelf : ITransformable<TSelf>
 	/// Rotate <typeparamref name="TSelf"/> instance clockwisely.
 	/// </summary>
 	/// <returns>The result rotated.</returns>
-	public abstract ref readonly TSelf RotateClockwise();
+	[UnscopedRef]
+	public abstract ref TSelf RotateClockwise();
 
 	/// <summary>
 	/// Rotate <typeparamref name="TSelf"/> instance counter-clockwisely.
 	/// </summary>
 	/// <returns>The result rotated.</returns>
-	public abstract ref readonly TSelf RotateCounterclockwise();
+	[UnscopedRef]
+	public abstract ref TSelf RotateCounterclockwise();
 
 	/// <summary>
 	/// Rotate <typeparamref name="TSelf"/> instance 180 degrees.
 	/// </summary>
 	/// <returns>The result rotated.</returns>
-	public sealed ref readonly TSelf RotatePi() => ref RotateClockwise().RotateClockwise();
+	[UnscopedRef]
+	public virtual ref TSelf RotatePi() => ref RotateClockwise().RotateClockwise();
 
 	/// <summary>
 	/// Mirror <typeparamref name="TSelf"/> instance in left-right side.
 	/// </summary>
 	/// <returns>The result fliped.</returns>
-	public abstract ref readonly TSelf MirrorLeftRight();
+	[UnscopedRef]
+	public abstract ref TSelf MirrorLeftRight();
 
 	/// <summary>
 	/// Mirror <typeparamref name="TSelf"/> instance in top-bottom side.
 	/// </summary>
 	/// <returns>The result fliped.</returns>
-	public abstract ref readonly TSelf MirrorTopBottom();
+	[UnscopedRef]
+	public abstract ref TSelf MirrorTopBottom();
 
 	/// <summary>
 	/// Mirror <typeparamref name="TSelf"/> instance in diagonal.
 	/// </summary>
 	/// <returns>The result fliped.</returns>
-	public abstract ref readonly TSelf MirrorDiagonal();
+	[UnscopedRef]
+	public abstract ref TSelf MirrorDiagonal();
+
+	/// <summary>
+	/// Simply calls <see cref="MirrorDiagonal"/>.
+	/// </summary>
+	/// <returns>The result fliped.</returns>
+	[UnscopedRef]
+	public virtual ref TSelf Transpose() => ref MirrorDiagonal();
 
 	/// <summary>
 	/// Mirror <typeparamref name="TSelf"/> instance in anti-diagonal.
 	/// </summary>
 	/// <returns>The result fliped.</returns>
-	public abstract ref readonly TSelf MirrorAntidiagonal();
+	[UnscopedRef]
+	public abstract ref TSelf MirrorAntidiagonal();
 }
