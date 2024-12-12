67163807117706580e8f42890be10cc87d51f0c0
Kyouyama Kazusa <Kazusa0805@outlook.com>
2024-12-12 10:15:06 +0800

    Update code.

---
+++ src/Puzzles.Core/Meta/IBoard.cs
@@ -0,0 +1,17 @@
+namespace Puzzles.Meta;
+
+/// <summary>
+/// Represents a game board.
+/// </summary>
+public interface IBoard
+{
+	/// <summary>
+	/// Indicates the number of rows.
+	/// </summary>
+	public abstract int Rows { get; }
+
+	/// <summary>
+	/// Indicates the number of columns.
+	/// </summary>
+	public abstract int Columns { get; }
+}
