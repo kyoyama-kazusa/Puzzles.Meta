6aba16ef04260a3fe314a23485bf8c10042b3c13
Kyouyama Kazusa <Kazusa0805@outlook.com>
2024-12-17 20:36:27 +0800

    Move file.

---
+++ src/Puzzles.Core/Concepts/Direction.cs
@@ -0,0 +1,32 @@
+namespace Puzzles.Concepts;
+
+/// <summary>
+/// Represents a direction.
+/// </summary>
+public enum Direction
+{
+	/// <summary>
+	/// Indicates the placeholder of the direction.
+	/// </summary>
+	None = 0,
+
+	/// <summary>
+	/// Indicates the direction is up.
+	/// </summary>
+	Up,
+
+	/// <summary>
+	/// Indicates the direction is down.
+	/// </summary>
+	Down,
+
+	/// <summary>
+	/// Indicates the direction is left.
+	/// </summary>
+	Left,
+
+	/// <summary>
+	/// Indicates the diretcion is right.
+	/// </summary>
+	Right
+}
