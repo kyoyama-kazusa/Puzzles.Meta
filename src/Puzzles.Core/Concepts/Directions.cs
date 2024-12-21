dbcf336978075052ee82d7f7a519d544fe833dd6
Kyouyama Kazusa <kazusa0805@outlook.com>
2024-12-21 10:37:48 +0800

    Add type aliases.

---
+++ src/Puzzles.Core/Concepts/Directions.cs
@@ -0,0 +1,14 @@
+﻿namespace Puzzles.Concepts;
+
+/// <summary>
+/// Provides a list of fields of type <see cref="Direction"/>.
+/// </summary>
+/// <seealso cref="Direction"/>
+public static class Directions
+{
+	/// <summary>
+	/// Represents max value of <see cref="Direction"/>.
+	/// </summary>
+	/// <seealso cref="Direction"/>
+	public const Direction MaxValue = (Direction)byte.MaxValue;
+}
