6102d2c1982684d64b5ada6aea0788c6f8983823
Kyouyama Kazusa <Kazusa0805@outlook.com>
2024-12-24 17:45:13 +0800

    Update code.

---
+++ src/Puzzles.Core/Measuring/DistanceType.cs
@@ -0,0 +1,22 @@
+namespace Puzzles.Measuring;
+
+/// <summary>
+/// Represents a type of distance to be measured.
+/// </summary>
+public enum DistanceType
+{
+	/// <summary>
+	/// Indicates Euclid distance.
+	/// </summary>
+	Euclid,
+
+	/// <summary>
+	/// Indicates Manhattan distance.
+	/// </summary>
+	Manhattan,
+
+	/// <summary>
+	/// Indicates solved distance.
+	/// </summary>
+	Solved
+}
