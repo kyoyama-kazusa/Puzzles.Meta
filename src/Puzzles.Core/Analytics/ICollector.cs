83ae6e5e2d47fd5b45864a31817f27b9c9759296
Kyouyama Kazusa <Kazusa0805@outlook.com>
2024-12-27 14:54:58 +0800

    Update variable name.

--- src/Puzzles.Core/Analytics/ICollector.cs
+++ src/Puzzles.Core/Analytics/ICollector.cs
@@ -10,10 +10,10 @@ public interface ICollector<TBoard, TMatch>
 	where TMatch : IEquatable<TMatch>, IEqualityOperators<TMatch, TMatch, bool>
 {
 	/// <summary>
-	/// Try to find all possible steps appeared in the grid; if no steps found, an empty array will be returned.
+	/// Try to find all possible steps appeared in the board; if no steps found, an empty array will be returned.
 	/// </summary>
-	/// <param name="grid">The grid.</param>
+	/// <param name="board">The board.</param>
 	/// <param name="cancellationToken">The cancellation token that can cancel the current task.</param>
 	/// <returns>All matched items.</returns>
-	public abstract ReadOnlySpan<TMatch> Collect(TBoard grid, CancellationToken cancellationToken = default);
+	public abstract ReadOnlySpan<TMatch> Collect(TBoard board, CancellationToken cancellationToken = default);
 }
