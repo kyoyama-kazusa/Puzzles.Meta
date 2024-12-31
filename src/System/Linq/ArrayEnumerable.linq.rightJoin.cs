db284678a94b2896fbc86327deb5da61857745ff
Kyouyama Kazusa <Kazusa0805@outlook.com>
2024-12-31 18:00:36 +0800

    Update code.

---
+++ src/System/Linq/ArrayEnumerable.linq.rightJoin.cs
@@ -0,0 +1,48 @@
+namespace System.Linq;
+
+public partial class ArrayEnumerable
+{
+	/// <inheritdoc cref="IRightJoinMethod{TSelf, TSource}.RightJoin{TInner, TKey, TResult}(IEnumerable{TInner}, Func{TSource, TKey}, Func{TInner, TKey}, Func{TSource, TInner, TResult})"/>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static ReadOnlySpan<TResult?> RightJoin<TOuter, TInner, TKey, TResult>(
+		this ReadOnlySpan<TOuter> outer,
+		ReadOnlySpan<TInner> inner,
+		Func<TOuter, TKey> outerKeySelector,
+		Func<TInner, TKey> innerKeySelector,
+		Func<TOuter?, TInner, TResult?> resultSelector
+	) where TKey : notnull => RightJoin(outer, inner, outerKeySelector, innerKeySelector, resultSelector, null);
+
+	/// <inheritdoc cref="IRightJoinMethod{TSelf, TSource}.RightJoin{TInner, TKey, TResult}(IEnumerable{TInner}, Func{TSource, TKey}, Func{TInner, TKey}, Func{TSource, TInner, TResult}, IEqualityComparer{TKey}?)"/>
+	public static ReadOnlySpan<TResult?> RightJoin<TOuter, TInner, TKey, TResult>(
+		this ReadOnlySpan<TOuter> outer,
+		ReadOnlySpan<TInner> inner,
+		Func<TOuter, TKey> outerKeySelector,
+		Func<TInner, TKey> innerKeySelector,
+		Func<TOuter?, TInner, TResult?> resultSelector,
+		IEqualityComparer<TKey>? comparer
+	)
+		where TKey : notnull
+	{
+		comparer ??= EqualityComparer<TKey>.Default;
+
+		var outerLookup = outer.ToLookup(outerKeySelector, comparer);
+		var result = new List<TResult?>();
+		foreach (var innerElement in inner)
+		{
+			var key = innerKeySelector(innerElement);
+			var outerElements = outerLookup[key];
+			if (outerElements.Any())
+			{
+				foreach (var outerElement in outerElements)
+				{
+					result.AddRef(resultSelector(outerElement, innerElement));
+				}
+			}
+			else
+			{
+				result.AddRef(resultSelector(default, innerElement));
+			}
+		}
+		return result.AsSpan();
+	}
+}
