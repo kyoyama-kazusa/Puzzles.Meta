799a22b8b5565ebf453071277a27b12b8784cfa3
Kyouyama Kazusa <Kazusa0805@outlook.com>
2025-02-11 20:21:43 +0800

    Update code.

--- src/System/Linq/SpanEnumerable.static.range.cs
+++ src/System/Linq/SpanEnumerable.static.range.cs
@@ -13,21 +13,34 @@ public partial class SpanEnumerable
 	public static ReadOnlySpan<int> Range(int start, int count) => Range(start, count, static previous => previous + 1);
 
 	/// <summary>
+	/// Create a range of variables that starts with <see cref="INumberBase{TSelf}.Zero"/> from type <typeparamref name="TInteger"/>,
+	/// incrementing values and putting them into the other positions.
+	/// </summary>
+	/// <typeparam name="TInteger">The type of integer.</typeparam>
+	/// <param name="count">The number of elements created.</param>
+	/// <returns>The result sequence [0, 1, 2, 3, ..].</returns>
+	public static ReadOnlySpan<TInteger> Range<TInteger>(int count) where TInteger : IBinaryInteger<TInteger>
+	{
+		var one = TInteger.CreateChecked(1);
+		return Range(TInteger.Zero, count, previous => previous + one);
+	}
+
+	/// <summary>
 	/// Create a range of variables that starts with the specified value, and iterates the value to create followed values.
 	/// </summary>
 	/// <typeparam name="T">The type of the target value.</typeparam>
 	/// <param name="start">The start value.</param>
 	/// <param name="count">The number of elements created.</param>
-	/// <param name="nextCreator">The creator method that create a value from the previous value.</param>
+	/// <param name="iterator">The creator method that create a value from the previous value.</param>
 	/// <returns>The result sequence.</returns>
-	public static ReadOnlySpan<T> Range<T>(T start, int count, Func<T, T> nextCreator)
+	public static ReadOnlySpan<T> Range<T>(T start, int count, Func<T, T> iterator)
 	{
 		var result = new T[count];
 		result[0] = start;
 
 		for (var i = 1; i < count; i++)
 		{
-			result[i] = nextCreator(result[i - 1]);
+			result[i] = iterator(result[i - 1]);
 		}
 		return result;
 	}
