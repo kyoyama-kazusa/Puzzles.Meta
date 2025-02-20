52886715b549c38b5f8ff38404da2e5ebccf587b
Kyouyama Kazusa <Kazusa0805@outlook.com>
2025-02-20 14:09:38 +0800

    Implement async version.

--- src/System/GlobalUsings.cs
+++ src/System/GlobalUsings.cs
@@ -20,3 +20,5 @@ global using System.Runtime.Versioning;
 global using System.Text;
 global using System.Text.Json.Serialization;
 global using System.Text.RegularExpressions;
+global using System.Threading;
+global using System.Threading.Tasks;
