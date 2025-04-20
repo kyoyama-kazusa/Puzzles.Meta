429668d70fc4e8c5cc6c77b210c4e900ac90b2e5
Kyouyama Kazusa <kazusa0805@outlook.com>
2025-04-20 09:32:17 +0800

    Update code.

--- src/System/Collections/Generic/IEnumerableExtensions.cs
+++ src/System/Collections/Generic/IEnumerableExtensions.cs
@@ -1,60 +1,73 @@
 namespace System.Collections.Generic;
 
 /// <summary>
-/// Provides with extension methods on <see cref="IEnumerable"/> and <see cref="IEnumerable{T}"/> instances.
+/// Provides with extension methods on <see cref="IEnumerable{T}"/> instances.
 /// </summary>
-/// <seealso cref="IEnumerable"/>
 /// <seealso cref="IEnumerable{T}"/>
 public static class IEnumerableExtensions
 {
-	/// <summary>
-	/// Provides extension members on <see cref="IEnumerable"/>.
-	/// </summary>
-	extension(IEnumerable @this)
-	{
-		/// <summary>
-		/// Indicates the length of the sequence.
-		/// </summary>
-		public int Length
-		{
-			get
-			{
-				if (@this is ICollection collection)
-				{
-					return collection.Count;
-				}
-
-				var (count, enumerator) = (0, @this.GetEnumerator());
-				try
-				{
-					checked
-					{
-						while (enumerator.MoveNext())
-						{
-							count++;
-						}
-					}
-					return count;
-				}
-				finally
-				{
-					if (enumerator is IDisposable disposable)
-					{
-						disposable.Dispose();
-					}
-				}
-			}
-		}
-	}
-
 	/// <summary>
 	/// Provides extension members on <see cref="IEnumerable{T}"/>.
 	/// </summary>
 	extension<T>(IEnumerable<T> @this)
 	{
 		/// <summary>
+		/// Indicates whether the sequence has no elements.
+		/// </summary>
+		public bool IsEmpty => @this.Any();
+
+		/// <summary>
 		/// Indicates the length of the sequence.
 		/// </summary>
 		public int Length => @this.Count();
+
+
+		/// <summary>
+		/// Gets the element at the specified index.
+		/// </summary>
+		/// <param name="index">The index.</param>
+		/// <returns>The element.</returns>
+		public T this[int index] => @this.ElementAt(index);
+
+		/// <inheritdoc cref="get_Item{T}(IEnumerable{T}, int)"/>
+		public T this[Index index] => @this.ElementAt(index);
+
+		/// <summary>
+		/// Gets the slice of the specified range.
+		/// </summary>
+		/// <param name="range">The range.</param>
+		/// <returns>The slice values.</returns>
+		public IEnumerable<T> this[Range range]
+		{
+			get
+			{
+				var (offset, length) = range.GetOffsetAndLength(@this.Length);
+				return @this.Slice(offset, length);
+			}
+		}
+
+
+		/// <summary>
+		/// Slices the current sequence.
+		/// </summary>
+		/// <param name="start">The start index.</param>
+		/// <param name="length">The desired length.</param>
+		/// <returns>A new sequence start at index <paramref name="start"/>.</returns>
+		/// <exception cref="InvalidOperationException">Throws when the sequence has no valid elements to be iterated.</exception>
+		public IEnumerable<T> Slice(int start, int length)
+		{
+			var skiped = @this.Skip(start);
+			if (skiped.IsEmpty)
+			{
+				throw new InvalidOperationException();
+			}
+
+			using var enumerator = skiped.GetEnumerator();
+			for (var i = 0; i < length; i++)
+			{
+				enumerator.MoveNext();
+				yield return enumerator.Current;
+			}
+		}
 	}
 }
