09ee4b957a051ad8d5c3c7d0b7b0d110500b02e6
Kyouyama Kazusa <kazusa0805@outlook.com>
2025-04-26 23:52:14 +0800

    Update code.

--- src/System/Numerics/BitOperationsExtensions.cs
+++ src/System/Numerics/BitOperationsExtensions.cs
@@ -835,6 +835,19 @@ public static partial class BitOperationsExtensions
 				return result;
 			}
 		}
+
+
+		/// <summary>
+		/// Creates a <see cref="BitCombinationGenerator{T}"/> instance that can generate a list of <see cref="long"/>
+		/// values that are all possibilities of combinations of integer values containing specified number of bits,
+		/// and the specified number of bits set 1.
+		/// </summary>
+		/// <param name="bitCount">Indicates how many bits should be enumerated.</param>
+		/// <param name="oneCount">Indicates how many bits set one contained in the value.</param>
+		/// <returns>A <see cref="BitCombinationGenerator{T}"/> instance.</returns>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static BitCombinationGenerator<TInteger> EnumerateOf(int bitCount, int oneCount)
+			=> new(bitCount, oneCount);
 	}
 
 	/// <summary>
