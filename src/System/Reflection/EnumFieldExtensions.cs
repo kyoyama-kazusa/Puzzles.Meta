cfee1e49018402fee39a4f57a02c185228b276f3
Kyouyama Kazusa <Kazusa0805@outlook.com>
2025-04-27 17:29:11 +0800

    Update code.

---
+++ src/System/Reflection/EnumFieldExtensions.cs
@@ -0,0 +1,22 @@
+namespace System.Reflection;
+
+/// <summary>
+/// Provides with extension methods on fields on enumeration types.
+/// </summary>
+public static class EnumFieldExtensions
+{
+	/// <summary>
+	/// Provides extension members on <typeparamref name="TEnum"/>,
+	/// where <typeparamref name="TEnum"/> satisfies <see cref="Enum"/> constraint.
+	/// </summary>
+	extension<TEnum>(TEnum) where TEnum : Enum
+	{
+		/// <summary>
+		/// Returns the field information of the specified field.
+		/// </summary>
+		/// <param name="field">The field.</param>
+		/// <returns>The <see cref="FieldInfo"/> instance.</returns>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static FieldInfo? FieldInfoOf(TEnum field) => typeof(TEnum).GetField(field.ToString());
+	}
+}
