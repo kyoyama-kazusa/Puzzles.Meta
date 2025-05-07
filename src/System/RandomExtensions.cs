5ee0a5afe345ebb61a17b6e23fcff503ea58f7ac
Kyouyama Kazusa <Kazusa0805@outlook.com>
2025-05-07 18:13:47 +0800

    Update code.

--- src/System/RandomExtensions.cs
+++ src/System/RandomExtensions.cs
@@ -26,5 +26,68 @@ public static class RandomExtensions
 			var randStdNormal = Math.Sqrt(-2D * Math.Log(u1)) * Math.Sin(Math.Tau * u2);
 			return mu + sigma * randStdNormal;
 		}
+
+		/// <summary>
+		/// Try to get one element from the collection.
+		/// </summary>
+		/// <typeparam name="T">The type of each element.</typeparam>
+		/// <param name="values">The values.</param>
+		/// <returns>The chosen element.</returns>
+		/// <exception cref="InvalidOperationException">Throws when the specified collection is empty.</exception>
+		public T Choose<T>(ReadOnlySpan<T> values) => @this.Choose(values, 1)[0];
+
+		/// <summary>
+		/// Try to get a list of elements from the collection.
+		/// </summary>
+		/// <typeparam name="T">The type of each element.</typeparam>
+		/// <param name="values">The values.</param>
+		/// <param name="count">The desired number of elements to get.</param>
+		/// <returns>The chosen elements.</returns>
+		/// <exception cref="InvalidOperationException">
+		/// Throws when the specified collection doesn't contain enough elements to get.
+		/// </exception>
+		public ReadOnlySpan<T> Choose<T>(ReadOnlySpan<T> values, int count)
+		{
+			InvalidOperationException.ThrowIfAssertionFailed(values.Length >= count);
+			if (values.Length == count)
+			{
+				return values;
+			}
+
+			var result = new T[count];
+			if (count <= values.Length >> 1)
+			{
+				var bitArray = new BitArray(values.Length);
+				for (var i = 0; i < count; i++)
+				{
+					var index = @this.Next(0, values.Length);
+					if (bitArray[index])
+					{
+						// Fallback.
+						do
+						{
+							index = (index + 1) % values.Length;
+						} while (bitArray[index]);
+					}
+					result[i] = values[index];
+					bitArray[i] = true;
+				}
+			}
+			else
+			{
+				var sequence = (stackalloc int[values.Length]);
+				for (var i = 0; i < values.Length; i++)
+				{
+					sequence[i] = i;
+				}
+				@this.Shuffle(sequence);
+
+				for (var i = 0; i < count; i++)
+				{
+					result[i] = values[sequence[i]];
+				}
+			}
+			return result;
+		}
 	}
 }
