b60fe9690e43592e72ff55fd663741be59eff759
Kyouyama Kazusa <Kazusa0805@outlook.com>
2025-05-12 17:45:03 +0800

    Update code.

---
+++ src/System/Numerics/FallbackConstants.cs
@@ -0,0 +1,20 @@
+namespace System.Numerics;
+
+/// <summary>
+/// Provides with fallback constants that will be used for checking target values,
+/// e.g. method <see cref="BitOperations.TrailingZeroCount(int)"/>.
+/// </summary>
+/// <seealso cref="BitOperations.TrailingZeroCount(int)"/>
+[SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>")]
+public static class FallbackConstants
+{
+	/// <summary>
+	/// Indicates the fallback value is for <see cref="int"/> and <see cref="uint"/> type.
+	/// </summary>
+	public const int @int = 32;
+
+	/// <summary>
+	/// Indicates the fallback value is for <see cref="long"/> and <see cref="ulong"/> type.
+	/// </summary>
+	public const int @long = 64;
+}
