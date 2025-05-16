8455acbfd3092aee116d56bf73910e54bb0b6f1a
Kyouyama Kazusa <Kazusa0805@outlook.com>
2025-05-16 15:13:31 +0800

    Update code.

---
+++ src/System/GenericExtensions.cs
@@ -0,0 +1,24 @@
+namespace System;
+
+/// <summary>
+/// Provides with extension methods on generic type.
+/// </summary>
+public static class GenericExtensions
+{
+	/// <summary>
+	/// Provides extension members on <typeparamref name="T"/>,
+	/// where <typeparamref name="T"/> satisfies <see langword="struct"/> constraint.
+	/// </summary>
+	extension<T>(T) where T : struct, allows ref struct
+	{
+		/// <summary>
+		/// Represents null reference of the type <typeparamref name="T"/>.
+		/// </summary>
+		[SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>")]
+		public static ref T nullref
+		{
+			[MethodImpl(MethodImplOptions.AggressiveInlining)]
+			get => ref Unsafe.NullRef<T>();
+		}
+	}
+}
