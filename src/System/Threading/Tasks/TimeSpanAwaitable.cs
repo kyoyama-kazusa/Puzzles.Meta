f8c7d54d3a3a89f1ba3028603a7dcdd0f9b3eda2
Kyouyama Kazusa <kazusa0805@outlook.com>
2025-05-17 23:58:22 +0800

    Update code.

--- src/System/Threading/Tasks/TimeSpanAwaitable.cs
+++ src/System/Threading/Tasks/TimeSpanAwaitable.cs
@@ -7,10 +7,40 @@ namespace System.Threading.Tasks;
 public static class TimeSpanAwaitable
 {
 	/// <summary>
-	/// Creates a <see cref="TimeSpanAwaiter"/> instance used for <see langword="await"/> expressions.
+	/// Provides extension members on <see cref="TimeSpan"/>.
 	/// </summary>
-	/// <param name="this">The time span.</param>
-	/// <returns>A <see cref="TimeSpanAwaiter"/> instance.</returns>
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public static TimeSpanAwaiter GetAwaiter(this TimeSpan @this) => new(Task.Delay(@this).GetAwaiter());
+	extension(TimeSpan @this)
+	{
+		/// <summary>
+		/// Creates a <see cref="Awaiter"/> instance used for <see langword="await"/> expressions.
+		/// </summary>
+		/// <returns>A <see cref="Awaiter"/> instance.</returns>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public Awaiter GetAwaiter() => new(Task.Delay(@this).GetAwaiter());
+	}
+
+
+	/// <summary>
+	/// Represents an awaiter for <see cref="TimeSpan"/> instance.
+	/// </summary>
+	/// <param name="_awaiter">The base awaiter instance.</param>
+	/// <seealso cref="TimeSpan"/>
+	public readonly struct Awaiter(TaskAwaiter _awaiter) : INotifyCompletion
+	{
+		/// <inheritdoc cref="TaskAwaiter.IsCompleted"/>
+		public bool IsCompleted
+		{
+			[MethodImpl(MethodImplOptions.AggressiveInlining)]
+			get => _awaiter.IsCompleted;
+		}
+
+
+		/// <inheritdoc cref="TaskAwaiter.GetResult"/>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public void GetResult() => _awaiter.GetResult();
+
+		/// <inheritdoc cref="TaskAwaiter.OnCompleted(Action)"/>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public void OnCompleted(Action continuation) => _awaiter.OnCompleted(continuation);
+	}
 }
