cb63f2166fc7602039e0d2780e0aed78ab5a6e7c
Kyouyama Kazusa <Kazusa0805@outlook.com>
2025-05-19 11:19:01 +0800

    Update code.

--- src/System/IO/FileExtensions.cs
+++ src/System/IO/FileExtensions.cs
@@ -18,11 +18,44 @@ public static class FileExtensions
 	extension(File)
 	{
 		/// <summary>
+		/// Append a new line into the file. If file doesn't exist, create a new file and append the line.
+		/// </summary>
+		/// <param name="path">The file path.</param>
+		/// <param name="line">The line.</param>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static void AppendLine(string path, string line) => File.AppendAllText(path, $"{line}{Environment.NewLine}");
+
+		/// <summary>
 		/// Determines whether the specified file name is valid.
 		/// </summary>
 		/// <param name="fileName">The file name to be checked.</param>
 		/// <returns>A <see cref="bool"/> result indicating that.</returns>
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static bool IsValidFileName(string fileName) => !fileName.Span.ContainsAny(InvalidCharacters);
+
+		/// <summary>
+		/// Reads the lines of file, with an option that can skip for the first line.
+		/// </summary>
+		/// <param name="path">The file path.</param>
+		/// <param name="skipFirstLine">Indicates whether the first line should be skipped.</param>
+		/// <returns>An enumerable sequence of lines.</returns>
+		public static IEnumerable<string> ReadLines(string path, bool skipFirstLine)
+		{
+			var firstLine = true;
+			foreach (var line in File.ReadLines(path))
+			{
+				if (firstLine)
+				{
+					firstLine = false;
+
+					if (skipFirstLine)
+					{
+						continue;
+					}
+				}
+
+				yield return line;
+			}
+		}
 	}
 }
