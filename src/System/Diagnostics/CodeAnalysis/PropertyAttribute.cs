9e7baf15960632958624915e8ab7f5ba29059548
Kyouyama Kazusa <Kazusa0805@outlook.com>
2025-05-23 14:10:54 +0800

    Update code.

--- src/System/Diagnostics/CodeAnalysis/PropertyAttribute.cs
+++
@@ -1,7 +0,0 @@
-namespace System.Diagnostics.CodeAnalysis;
-
-/// <summary>
-/// Indicates the backing generated member of this primary constructor is a property.
-/// </summary>
-[AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
-public sealed class PropertyAttribute : ParameterTargetAttribute;
