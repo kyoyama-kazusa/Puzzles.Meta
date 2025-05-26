0171a610ee17f0a711e949da829bc9311aa75517
Kyouyama Kazusa <Kazusa0805@outlook.com>
2025-05-26 11:47:00 +0800

    Update code.

--- src/System/MathExtensions.cs
+++ src/System/MathExtensions.cs
@@ -17,7 +17,7 @@ public static class MathExtensions
 		/// <param name="a">The first value.</param>
 		/// <param name="b">The second value.</param>
 		/// <param name="c">The third value.</param>
-		/// <returns>Which is the minimal value.</returns>
+		/// <returns>The minimal one.</returns>
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static int Min(int a, int b, int c) => a < b ? a < c ? a : c : b < c ? b : c;
 
@@ -27,8 +27,52 @@ public static class MathExtensions
 		/// <param name="a">The first value.</param>
 		/// <param name="b">The second value.</param>
 		/// <param name="c">The third value.</param>
-		/// <returns>Which is the maximum value.</returns>
+		/// <returns>The maximum one.</returns>
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static int Max(int a, int b, int c) => a > b ? a > c ? a : c : b > c ? b : c;
+
+		/// <summary>
+		/// Gets the minimal one of two values.
+		/// </summary>
+		/// <typeparam name="T">The type of value.</typeparam>
+		/// <param name="a">The first value.</param>
+		/// <param name="b">The second value.</param>
+		/// <returns>The minimal one.</returns>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static T Min<T>(T a, T b) where T : IComparisonOperators<T, T, bool> => a < b ? a : b;
+
+		/// <summary>
+		/// Gets the minimal one of 3 values.
+		/// </summary>
+		/// <typeparam name="T">The type of value.</typeparam>
+		/// <param name="a">The first value.</param>
+		/// <param name="b">The second value.</param>
+		/// <param name="c">The third value.</param>
+		/// <returns>The minimal one.</returns>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static T Min<T>(T a, T b, T c) where T : IComparisonOperators<T, T, bool>
+			=> a < b ? a < c ? a : c : b < c ? b : c;
+
+		/// <summary>
+		/// Gets the maximal one of two values.
+		/// </summary>
+		/// <typeparam name="T">The type of value.</typeparam>
+		/// <param name="a">The first value.</param>
+		/// <param name="b">The second value.</param>
+		/// <returns>The maximal one.</returns>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static T Max<T>(T a, T b) where T : IComparisonOperators<T, T, bool> => a > b ? a : b;
+
+		/// <summary>
+		/// Gets the maximal one of 3 values.
+		/// </summary>
+		/// <typeparam name="T">The type of value.</typeparam>
+		/// <param name="a">The first value.</param>
+		/// <param name="b">The second value.</param>
+		/// <param name="c">The third value.</param>
+		/// <returns>The maximal one.</returns>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static T Max<T>(T a, T b, T c) where T : IComparisonOperators<T, T, bool>
+			=> a > b ? a > c ? a : c : b > c ? b : c;
 	}
 }
