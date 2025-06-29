abe710606f63219745ff8942a7804099dda137b6
Kyouyama Kazusa <kazusa0805@outlook.com>
2025-06-30 00:42:03 +0800

    Update rank calculation rule.

--- src/System/Collections/Generic/SortedSetExtensions.cs
+++ src/System/Collections/Generic/SortedSetExtensions.cs
@@ -16,7 +16,7 @@ public static class SortedSetExtensions
 		/// </summary>
 		/// <param name="values">The elements to be added.</param>
 		/// <returns>The number of elements successfully to be added.</returns>
-		public int AddRange(ReadOnlySpan<T> values)
+		public int AddRange(params ReadOnlySpan<T> values)
 		{
 			var result = 0;
 			foreach (var element in values)
